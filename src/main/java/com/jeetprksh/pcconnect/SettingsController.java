6ae2ce9f21add57cddcb28cef9286078096c1c7f
Jeet Prakash <jeet1989a@gmail.com>
2020-05-31 19:44:09 +0530

    Enabled selection of download directory

--- src/main/java/com/jeetprksh/pcconnect/SettingsController.java
+++ src/main/java/com/jeetprksh/pcconnect/SettingsController.java
@@ -1,10 +1,13 @@
 package com.jeetprksh.pcconnect;
 
+import java.io.File;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 
 public class SettingsController {
 
@@ -15,7 +18,12 @@ public class SettingsController {
   @FXML private Button save;
 
   public void browseDownloadDirectory() {
-    logger.info("browse directory");
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    File selectedDirectory = directoryChooser.showDialog(null);
+    if (!Objects.isNull(selectedDirectory)) {
+      downloadDirectory.setText(selectedDirectory.getAbsolutePath());
+    }
   }
 
   public void saveSettings() {
